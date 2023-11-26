459090a58f29618e99dbf052a77ad81bb058f9c2
Jasper Wang <jasper8777@icloud.com>
2023-11-25 18:46:01 -0800

    Move unit test files into files directory

---
+++ tests/test_parse_yarn_content.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from dialogues import parse_yarn_content
+
+# from unittest.mock import patch
+
+
+class TestParseYarnContent(TestCase):
+    def test_no_options(self):
+        self.fail()
+
+    def test_two_options(self):
+        self.fail()
