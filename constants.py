1b2d6ca10a4224d607235a85b20a9a75c46792ff
Jasper Wang <jasper8777@icloud.com>
2023-11-26 20:45:57 -0800

    refactor and create constants.py to store constants value

---
+++ constants.py
@@ -0,0 +1,16 @@
+def TEXT_FLAG():
+    return {"CONTENT_START_FLAG": "---", "OPTION_FLAG": "-> "}
+
+
+def TEXT_COLOR():
+    return {
+        "HEADER": "\033[95m",
+        "NPC": "\033[94m",
+        "OKCYAN": "\033[96m",
+        "PLAYER": "\033[92m",
+        "YELLOW": "\033[93m",
+        "ENEMY": "\033[91m",
+        "NORMAL": "\033[0m",
+        "BOLD": "\033[1m",
+        "UNDERLINE": "\033[4m",
+    }
